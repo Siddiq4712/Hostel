dd0e39acf72cc34aeec01a3b9c8f51bcaf53148f
Siddiq4712 <siddiqabubacker148@gmail.com>
2025-09-19 10:36:41 +0530

    Database connected  successfully using sequelize

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,31 +1,26 @@
-// config/db.js
-import mysql from 'mysql2';
+// utils/db.js
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    connectionLimit: 10
-});
+    dialect: 'mysql'
+  }
+);
 
-const db = pool.promise();
-
-// test DB connection at startup
-async function testConnection() {
-    try {
-        await db.query('SELECT 1');
-        console.log('✅ Database connected successfully!');
-    } catch (err) {
-        console.error('❌ DB connection failed:', err.message);
-        process.exit(1); // stop server if DB fails
-    }
+try {
+  await sequelize.authenticate();
+  console.log('✅ Database connected successfully!');
+} catch (err) {
+  console.error('❌ DB connection failed:', err.message);
+  process.exit(1);
 }
 
-testConnection();
-
-export default db;
+export default sequelize;
